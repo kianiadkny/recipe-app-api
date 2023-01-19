da68188dc63813961d9b9c1d04bd955d3b4b40e1
David Kiania <davidkiania@Davids-MacBook-Pro.local>
2023-01-19 14:19:29 +0300

    Adding wait time for DB and tests

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,26 @@
+'''
+Django command to wait for the pgsql database to be ready
+'''
+import time 
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    ''' Django command to wait for database to be ready'''
+
+    def handle(self, *args, **options):
+        '''Entry point for command'''
+        self.stdout.write('Waiting for postgres database ...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable,waiting for 1 second')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available for use!'))
